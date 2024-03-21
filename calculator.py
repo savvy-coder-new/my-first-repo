a3ecd76fd6e3ec3d00235eed00c763a12ef0cdcd
savvy-coder-new <164303408+savvy-coder-new@users.noreply.github.com>
2024-03-21 07:54:17 +0000

    add: print sum

--- calculator.py
+++ calculator.py
@@ -1,2 +1,4 @@
-a = int(input("Enter first number"))
-b = int(input("Enter second number"))
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum is "+ str(a+b))
